6c5845aee8e427366e559ea545389f8d53d1dc64
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-06-09 13:23:01 +0500

    Refactor App, User, and Users components to utilize class-based structure and implement context for user management. Enhance Users component with error handling and state management for user visibility. Clean up unused code and comments for clarity.

--- 14-An-Alternative-Way-Of-Building-Components-Class-based-Components/00-class-base-components/src/App.js
+++ 14-An-Alternative-Way-Of-Building-Components-Class-based-Components/00-class-base-components/src/App.js
@@ -1,10 +1,35 @@
-import Users from './components/Users';
+// import UserFinder from "./components/UserFinder";
+// // import Users from './components/Users';
+
+// function App() {
+//   return (
+//     <div>
+//       <UserFinder />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Context
+import UsersContext from "./store/users-context";
+import UserFinder from "./components/UserFinder";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS,
+  };
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 

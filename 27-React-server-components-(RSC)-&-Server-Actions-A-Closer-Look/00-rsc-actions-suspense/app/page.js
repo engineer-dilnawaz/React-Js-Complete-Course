749d5f6d454f4650c878ccac641c2771068f51cf
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-06-22 12:10:50 +0500

    React + Next JS Advance topics section cover

--- 27-React-server-components-(RSC)-&-Server-Actions-A-Closer-Look/00-rsc-actions-suspense/app/page.js
+++ 27-React-server-components-(RSC)-&-Server-Actions-A-Closer-Look/00-rsc-actions-suspense/app/page.js
@@ -1,7 +1,35 @@
-export default function Home() {
+import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
+import RSCDemo from "@/components/RSCDemo";
+import ServerActionsDemo from "@/components/ServerActionsDemo";
+import UsePromiseDemo from "@/components/UsePromisesDemo";
+import { Suspense } from "react";
+import fs from "node:fs/promises";
+import ErrorBoundary from "@/components/ErrorBoundary";
+
+export default async function Home() {
+  const fetchUserPromise = new Promise((resolve, reject) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      // resolve(users);
+      reject(new Error("Error while fetching users data."));
+    }, 2000)
+  );
   return (
     <main>
-      <p>Let's go!</p>
+      {/* <RSCDemo /> */}
+      {/* <ClientDemo>
+        <RSCDemo />
+      </ClientDemo> */}
+      {/* <DataFetchingDemo /> */}
+      {/* <ServerActionsDemo /> */}
+
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo userPromise={fetchUserPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
